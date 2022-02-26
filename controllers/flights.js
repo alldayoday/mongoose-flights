7cd0566f97997fb92159bb920e34220d3a0c1729
Erin ODea <odea.erin@gmail.com>
2022-02-25 18:51:49 -0600

    redirecting to movies cant find movies

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,11 +1,20 @@
-// import { Flight } from "../models/flight.js"
+import { Flight } from "../models/flight.js"
 
 
 function newFlight(req, res) {
   res.render('flights/new')
 }
 
+function create(req, res) {
+  const flight = new Flight(req.body)
+  flight.save(function(err) {
+		if (err) {return res.redirect('/flights/new')
+}
+    res.redirect('/flights/new')
+  })
+}
 
 export {
   newFlight as new,
+  create,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,10 @@
-
 import { Router } from 'express'
 const router = Router()
 import * as flightsCtrl from '../controllers/flights.js'
 
-// GET /movies/new
+
 router.get('/new', flightsCtrl.new)
+router.get('/', flightsCtrl.create)
 
 export {
 	router
